ab1e25b5817a88ae2b9c9e6712e0ece69ca5a339
Neill Turner <neillwturner@gmail.com>
2021-07-27 14:54:56 +0100

    remove separate 443 lambda

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ resource "aws_cloudwatch_event_rule" "populate_nlb_tg_with_rds_event" {
   description         = "Populate NLB Target Group with RDS IP"
   schedule_expression = var.schedule_expression
   depends_on          = [
-                  aws_lambda_function.populate_nlb_tg_with_rds_updater_80,
-                  aws_lambda_function.populate_nlb_tg_with_rds_updater_443
+                  aws_lambda_function.populate_nlb_tg_with_rds_updater_80
                 ]
 }
 
@@ -16,12 +15,6 @@ resource "aws_cloudwatch_event_target" "populate_nlb_tg_with_rds_event_lambda_80
   arn       = aws_lambda_function.populate_nlb_tg_with_rds_updater_80.arn
 }
 
-resource "aws_cloudwatch_event_target" "populate_nlb_tg_with_rds_event_lambda_443_target" {
-  target_id = "populate-nlb-tg-with-rds-event-lambda-443-target"
-  rule      = aws_cloudwatch_event_rule.populate_nlb_tg_with_rds_event.name
-  arn       = aws_lambda_function.populate_nlb_tg_with_rds_updater_443.arn
-}
-
 # permissions to each Lambda function to allow them to be triggered by Cloudwatch
 resource "aws_lambda_permission" "allow_cloudwatch_80" {
   statement_id  = "AllowExecutionFromCloudWatch"
@@ -30,15 +23,6 @@ resource "aws_lambda_permission" "allow_cloudwatch_80" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.populate_nlb_tg_with_rds_event.arn
 }
-
-resource "aws_lambda_permission" "allow_cloudwatch_443" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.populate_nlb_tg_with_rds_updater_443.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.populate_nlb_tg_with_rds_event.arn
-}
-
 
 # IAM Role for Lambda function
 resource "aws_iam_role_policy" "populate_nlb_tg_with_rds_lambda" {
@@ -141,23 +125,4 @@ resource "aws_lambda_function" "populate_nlb_tg_with_rds_updater_80" {
   }
 }
 
-
-resource "aws_lambda_function" "populate_nlb_tg_with_rds_updater_443" {
-  filename         = data.archive_file.lambda_function.output_path
-  function_name    = "populate_nlb_tg_with_rds_updater_443"
-  role             = aws_iam_role.populate_nlb_tg_with_rds_lambda.arn
-  handler          = "populate_nlb_tg_with_rds.handler"
-  source_code_hash = data.archive_file.lambda_function.output_base64sha256
-  runtime          = "python3.8"
-  memory_size      = 128
-  timeout          = 300
-
-  environment {
-    variables = {
-      RDS_DNS_NAME                      = element(split(":", var.rds_dns_name), 0)
-      NLB_TG_ARN                        = var.nlb_tg_arn
-      MAX_LOOKUP_PER_INVOCATION         = var.max_lookup_per_invocation
-    }
-  }
-}
 
--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "lambda_80_arn" {
 output "lambda_80_function_name" {
   value = aws_lambda_function.populate_nlb_tg_with_rds_updater_80.function_name
 }
-
-output "lambda_443_arn" {
-  value = aws_lambda_function.populate_nlb_tg_with_rds_updater_443.arn
-}
-
-output "lambda_443_function_name" {
-  value = aws_lambda_function.populate_nlb_tg_with_rds_updater_443.function_name
-}
